a833878128c86adc828603c454f4ba49ecca7cb5
new-wang <1569566735@qq.com>
2022-11-01 15:22:13 +0800

    +：面包屑导航

--- src/layouts/components/Breadcrumb/index.vue
+++ src/layouts/components/Breadcrumb/index.vue
@@ -1,13 +1,38 @@
 <template>
-    <div>
-        breadcrumb
-    </div>
+    <el-breadcrumb class="app-breadcrumb" separator="/">
+        <transition-group name="breadcrumb">
+            <el-breadcrumb-item v-for="(item,index) in levelList" :key = "index">
+                <span> {{ item.meta.title }} </span>
+            </el-breadcrumb-item>
+        </transition-group>
+    </el-breadcrumb>
 </template>
 
 <script setup>
+import { ref,watch } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const levelList = ref(null);
+const router = useRouter();
+const route = useRoute();
+const getBreadcrumb = ()=>{
+    let matched = route.matched.filter((item) => item.meta && item.meta.title);
+    // const first = matched[0];
+
+    levelList.value = matched.filter((item) => item.meta && item.meta.breadcrumb !== false );
+}
+getBreadcrumb()
+watch(route,getBreadcrumb)
+console.log('router :>> ', router);
+console.log('route :>> ', route);
 
 </script>
 
 <style lang="scss" scoped>
-
+.app-breadcrumb.el-breadcrumb{
+    display: inline-block;
+    font-size: 14px;
+    line-height: 50px;
+    margin-left: 8px;
+}
 </style>
